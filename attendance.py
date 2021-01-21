332ebb57f08d253f19c9cf4edd4fa3317447f570
Chris Pyles <cpyles@berkeley.edu>
2021-01-20 23:40:52 -0800

    restructure for zoom participants export

--- attendance.py
+++ attendance.py
@@ -1,8 +1,10 @@
 import os
 import sys
 import json
+import glob
 import tempfile
 import pandas as pd
+import numpy as np
 from otter.generate.token import APIClient
 
 # create and fill in this file
@@ -10,11 +12,14 @@ import config
 
 
 # set these 3 variableas
-ZOOM_REGISTRATION_PATH = "data/zoom_registration.csv"
-ATTENDANCE_LISTS_DIR = "data/zoom_attendance_data"
-QUIZ_DATA_DIR = "data/gs_quiz_data"
+# ZOOM_REGISTRATION_PATH = "data/zoom_registration.csv"
+# ATTENDANCE_LISTS_DIR = "data/zoom_attendance_data"
+ATTENDANCE_CSVS_DIR = "data/zoom_sp21"
+QUIZ_DATA_DIR = "data/gs_sp21"
 COURSE_ID = config.COURSE_ID
 ASSIGNMENT_ID = config.ASSIGNMENT_ID
+ATTENDANCE_MINUTES_MIN = 60
+QUIZ_SCORE_CUTOFF = 0.6
 
 # don't change these ones
 SUBMISSIONS = {}
@@ -22,12 +27,24 @@ EVERYONE_GETS_CREDIT = "--EVERYONE--"
 
 
 def main():
-    registration = pd.read_csv(ZOOM_REGISTRATION_PATH)
-    registration["Name"] = registration.apply(lambda row: row["First Name"] + " " + row["Last Name"], axis=1)
+    # registration = pd.read_csv(ZOOM_REGISTRATION_PATH)
+    # registration["Name"] = registration.apply(lambda row: row["First Name"] + " " + row["Last Name"], axis=1)
+    
+
+    emails = set()
+    quiz_dfs = []
+    for fn in sorted(os.listdir(QUIZ_DATA_DIR)):
+        fp = os.path.joni(QUIZ_DATA_DIR, fn)
+        df = pd.read_csv(fp)
+        emails |= df["Email"]
+        quiz_dfs.append(df)
+
+    emails = list(sorted(emails))
+    students = pd.DataFrame({"Email": emails})
 
     everyone = []
-    for i, fn in enumerate(sorted(os.listdir(ATTENDANCE_LISTS_DIR))):
-        fp = os.path.join(ATTENDANCE_LISTS_DIR, fn)
+    for i, fn in enumerate(sorted(os.listdir(ATTENDANCE_CSVS_DIR))):
+        fp = os.path.join(ATTENDANCE_CSVS_DIR, fn)
         with open(fp) as f:
             attendees = f.readlines()
 
@@ -36,39 +53,53 @@ def main():
 
         else:
 
-            attendees = [att.strip() for att in attendees]
+            zoom_attendees = pd.read_csv(fp)
+            zoom_attendees = zoom_attendees[zoom_attendees["Total Duration (Minutes)"] >= ATTENDANCE_MINUTES_MIN]
+            # attendee_emails = pd.read_csv(fp)["User Email"]
+            # attendee_durations = pd.read_csv(fp)
+            # attendees = [att.strip() for att in attendees]
 
-            registration[f"attended_in_person_{fn}"] = registration["Name"].isin(attendees)
-            registration[f"attended_{fn}"] = registration[f"attended_in_person_{fn}"]
+            # attended = zoom_attendees["User Email"].isin(emails)
+
+            # registration[f"attended_in_person_{fn}"] = registration["Name"].isin(attendees)
+            # registration[f"attended_{fn}"] = registration[f"attended_in_person_{fn}"]
             
-            quiz_fp = os.path.join(QUIZ_DATA_DIR, os.path.splitext(fn)[0] + ".csv")
-            quiz_scores = pd.read_csv(quiz_fp)
+            # quiz_fp = os.path.join(QUIZ_DATA_DIR, os.path.splitext(fn)[0] + ".csv")
+            quiz_scores = quiz_dfs[i]
             quiz_total_points = quiz_scores["Max Points"].max()
 
             def get_quiz_score(row):
                 try:
-                    return quiz_scores[quiz_scores["Email"] == row["Email"]].iloc[0]["Total Score"] / quiz_total_points
+                    row = quiz_scores[quiz_scores["Email"] == row["Email"]].iloc[0]
+                    if row["Status"] == "Missing":
+                        return np.nan
+                    return row["Total Score"] / quiz_total_points
                 except:
-                    return 0
+                    return np.nan
 
-            registration[f"quiz_score_{fn}"] = registration.apply(get_quiz_score, axis=1)
+            # registration[f"quiz_score_{fn}"] = registration.apply(get_quiz_score, axis=1)
+            lec_quiz_scores = students.apply(get_quiz_score, axis=1)
+            students[f"quiz_score_{fn}"]  = lec_quiz_scores
+            students[f"attended_in_person_{fn}"] = students["Email"].isin(zoom_attendees["User Email"])
+            students[f"attended_{fn}"] = students[f"attended_in_person_{fn}"]
+            students.loc[~students[f"attended_in_person_{fn}"] & (students[f"quiz_score_{fn}"] >= QUIZ_SCORE_CUTOFF), f"attended_{fn}"] = True
 
-            registration.loc[~registration[f"attended_in_person_{fn}"] & (registration[f"quiz_score_{fn}"] >= 0.8), f"attended_{fn}"] = True        
+            # registration.loc[~registration[f"attended_in_person_{fn}"] & (registration[f"quiz_score_{fn}"] >= 0.8), f"attended_{fn}"] = True        
 
-            for _, row in registration.iterrows():
+            for _, row in students.iterrows():
                 if row["Email"] not in SUBMISSIONS:
                     SUBMISSIONS[row["Email"]] = []
                 SUBMISSIONS[row["Email"]].append({
                     "lecture_file": fn,
                     "attended": row[f"attended_{fn}"],
                     "attended_in_person": row[f"attended_in_person_{fn}"],
-                    "quiz_score": row[f"quiz_score_{fn}"] if not pd.isna(row[f"quiz_score_{fn}"]) else 0,
+                    "quiz_score": row[f"quiz_score_{fn}"],
                 })
 
-            if sum(registration[f"attended_{fn}"]) < len(attendees):
+            if sum(students[f"attended_{fn}"]) < len(attendees):
                 print(f"Some attendees in {fn} not found in registrants")
-                print([att for att in attendees if att not in registration["Name"].tolist()])
-                print(registration)
+                print(zoom_attendees[~zoom_attendees["User Email"].isin(students["Email"])])
+                print(students)
                 sys.exit(1)
 
 
